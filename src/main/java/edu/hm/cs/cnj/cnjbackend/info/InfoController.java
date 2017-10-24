b7b44466b495dee1e0709b677e00bfe22c5ab067
David Hanway <david.hanway@ars.de>
2017-10-24 16:23:11 +0200

    - greeting message no longer hard-coded

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -1,5 +1,6 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InfoController {
 
+    @Value("${greeting.message}")
+    private String message;
+
     @RequestMapping("/info")
     public Info greeting() {
         Info result = new Info();
-        result.setMessage("OK");
+        result.setMessage(message);
         return result;
     }
 
