0cabf90a1d0aa171ec24ab663535113d97b8d5b8
Michael Ziegler <ziegler0@hm.edu>
2017-12-07 11:21:34 +0100

    Configuration in Java-Code

--- src/main/java/edu/hm/cs/cnj/cnjbackend/CnjBackendApplication.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/CnjBackendApplication.java
@@ -2,12 +2,21 @@ package edu.hm.cs.cnj.cnjbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.Cloud;
+import org.springframework.cloud.CloudFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
 @EnableSwagger2
@@ -24,5 +33,34 @@ public class CnjBackendApplication {
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
 				.build();
+	}
+
+	@Configuration
+	@Profile("default") // or "!cloud"
+	public class LocalDataSourceConfiguration {
+
+		@Bean
+		@ConfigurationProperties("spring.datasource")
+		public DataSource dataSource() {
+			return DataSourceBuilder.create().build();
+		}
+
+	}
+
+	@Configuration
+	@Profile("cloud")
+	public class DataSourceConfiguration {
+
+		@Bean
+		public Cloud cloud() {
+			return new CloudFactory().getCloud();
+		}
+
+		@Bean
+		@ConfigurationProperties("spring.datasource")
+		public DataSource dataSource() {
+			return cloud().getSingletonServiceConnector(DataSource.class, null);
+		}
+
 	}
 }
