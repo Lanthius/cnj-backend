f9d4b1fe865cfd46b66e431057a0fa7894118c71
Michael Ziegler <ziegler0@hm.edu>
2017-12-12 20:12:19 +0100

    - Data initialization removed

--- src/main/java/edu/hm/cs/cnj/cnjbackend/CnjBackendApplication.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/CnjBackendApplication.java
@@ -2,18 +2,12 @@ package edu.hm.cs.cnj.cnjbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.sql.DataSource;
-
 
 @SpringBootApplication
 @EnableSwagger2
@@ -32,15 +26,4 @@ public class CnjBackendApplication {
 				.build();
 	}
 
-
-	@Configuration
-	@Profile("cloud")
-	public static class DataSourceConfig extends AbstractCloudConfig {
-
-		@Bean
-		DataSource reservationsPostgreSqlDb() {
-			return connectionFactory().dataSource("cleardb");
-		}
-
-	}
 }
